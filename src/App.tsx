986543a154160bf683b5dce11228d529cdc68e37
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-07 12:10:36 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const App = () => {
   return (
     <div>
-      <p className="text-9xl bg-gray-400 text-center">Hello</p>
+      <p className="text-9xl bg-gray-400 text-center">Hello World</p>
     </div>
   )
 }
