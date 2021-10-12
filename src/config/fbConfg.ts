2a8a21d84d530d488fe842c9963ecc52577c03c6
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-12 13:52:26 +0530

    update env

--- src/config/fbConfg.ts
+++ src/config/fbConfg.ts
@@ -10,6 +10,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
-
+console.log(firebaseConfig)
 initializeApp(firebaseConfig)
 export default getFirestore()
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import "./styles/main.css"
+import "./assets/css/main.css"
 import "./styles/reset.css"
 import UseTaskGlobalContext from "./context"
 
