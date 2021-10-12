0a89d61af1a16b6bb0ad7fa34a736733a1f20538
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-12 17:56:08 +0530

    added edit project option of project

---
+++ src/utils/editProjectItem.ts
@@ -0,0 +1,22 @@
+import { doc, setDoc } from "firebase/firestore"
+import db from "../config/fbConfg"
+import defaultData from "../data/defaultData"
+
+const editProjectItem = async (
+  input: string,
+  endProjectDate: Date,
+  projectId: string
+) => {
+  if (projectId && endProjectDate) {
+    const collectionRef = doc(db, "users", projectId)
+    const payload = {
+      ...defaultData,
+      projectName: input,
+      endProjectDate: new Date(endProjectDate),
+      updateDate: new Date(),
+    }
+    await setDoc(collectionRef, payload)
+  }
+}
+
+export default editProjectItem
